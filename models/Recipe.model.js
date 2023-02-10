27bbd8d10caaba8443b153fd653038fe9c7fd186
Cedric Chatellier <cedricchatellier95.com>
2023-02-10 16:45:47 +0100

    solved labs

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,27 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: String,
+  level: {
+    type: String,
+    enum: ['Easy Peasy','Amateur Chef','UltraPro Chef'],
+  },
+  ingredients: [ String ],
+  cuisine: String,
+  dishType: String,
+  image: {
+    type: String,
+    // default: 'https://images.media-allrecipes.com/images/75131.jpg',
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  creator: String,
+  created: {
+    type: Date,
+    // default: '2023-02-10',
+  },
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
